3bd3aa2c23eae929d9c6b46ad0d086f663274d4e
Toam10 <78689759+Toam10@users.noreply.github.com>
2022-01-17 02:16:19 +0200

    Update server.js

--- bank-api/server.js
+++ bank-api/server.js
@@ -8,6 +8,10 @@ const path = require("path");
 
 const { env } = process;
 const { PORT } = env;
+
+// can be const {PORT} = process.env
+// I did it to make it clearer maybe that was my bad sorry
+
 const app = express();
 
 app.use(cors());
@@ -17,3 +21,7 @@ app.use("/", rootRouter);
 
 
 app.listen(PORT, () => console.log(`Server is up and running on ${PORT}`));
+
+// I know that I did it please try to write this server alone that you will understand the process (: 
+
+
